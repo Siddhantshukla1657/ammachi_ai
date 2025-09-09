eb1506a4236374f5d20fa2684b3f5a4734f0becc
SM006 <shreyam1005@gmail.com>
2025-09-09 21:45:11 +0530

    change to packages in Schema

--- backend/models/Farmer.js
+++ backend/models/Farmer.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const FarmerSchema = new mongoose.Schema({
   farmerId: { type: String, required: true, unique: true, index: true }, 
