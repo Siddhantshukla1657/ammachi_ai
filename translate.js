be2ba775dab0c58f99536dbb9cf55c6d70578b42
Yashvi2874 <yashasvigupta28@gmail.com>
2025-09-10 10:27:44 +0530

    Last fix.

--- translate.js
+++
@@ -1,6 +0,0 @@
-const { GoogleGenerativeAI } = require('@google/generative-ai');
-const apiKey = process.env.API_KEY;
-const modelName = "gemini-2.5-flash"; // use this model name for requests
-
-const genAI = new GoogleGenerativeAI(apiKey);
-const model = genAI.getGenerativeModel({ model: modelName });
